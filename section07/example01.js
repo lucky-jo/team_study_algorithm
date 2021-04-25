01a5f2ab4322cc90597b9f0cb2ebfcdfe26a236a
Jaebock jo <last2599@gmail.com>
2021-04-25 17:04:07 +0900

    선택 정렬

---
+++ section07/example01.js
@@ -0,0 +1,13 @@
+function solution(arr) {
+  for (let i = 0; i < arr.length; i++) {
+    let idx = i;
+    for (let j = i + 1; j < arr.length; j++) {
+      if (arr[j] < arr[idx]) idx = j;
+    }
+    [arr[i], arr[idx]] = [arr[idx], arr[i]];
+  }
+  return arr.join(" ");
+}
+
+let arr = [13, 5, 11, 7, 23, 15];
+console.log(solution(arr));
