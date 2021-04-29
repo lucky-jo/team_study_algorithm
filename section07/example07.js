c8ab736c916de853d1f1987423c70aabb22cb20e
Jaebock jo <last2599@gmail.com>
2021-04-30 00:16:07 +0900

    좌표 정렬
    N개의 평면상의 좌표(x, y)가 주어지면 모든 좌표를 오름차순으로 정렬하는 프로그램을 작성하 세요. 정렬기준은 먼저 x값의 의해서 정렬하고, x값이 같을 경우 y값에 의해 정렬합니다.

---
+++ section07/example07.js
@@ -0,0 +1,17 @@
+function solution(arr) {
+  return arr
+    .sort((a, b) => {
+      if (a[0] === b[0]) return a[1] - b[1];
+      else return a[0] - b[0];
+    })
+    .join("\n");
+}
+
+let arr = [
+  [2, 7],
+  [1, 3],
+  [1, 2],
+  [2, 5],
+  [3, 6],
+];
+console.log(solution(arr));
