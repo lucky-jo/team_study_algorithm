d91137d879aa81371f73f26193809ddb2229aac3
Jaebock jo <last2599@gmail.com>
2021-05-17 04:20:05 +0900

    중복순열 구하기
    1부터 N까지 번호가 적힌 구슬이 있습니다. 이 중 중복을 허락하여 M번을 뽑아 일렬로 나열 하는 방법을 모두 출력합니다.

---
+++ section08/example08.js
@@ -0,0 +1,21 @@
+function solution(n, m) {
+  const arr = Array.from({ length: m }, () => 0);
+  const results = [];
+
+  function dfs(v = 0) {
+    if (v === m) {
+      results.push(arr.slice());
+    } else {
+      for (let i = 0; i < n; i++) {
+        arr[v] = i + 1;
+        dfs(v + 1);
+      }
+    }
+  }
+
+  dfs();
+  console.log(results.join("\n"));
+  return results.length;
+}
+
+console.log(solution(3, 2));
