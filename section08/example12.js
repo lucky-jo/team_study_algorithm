e76a28561e8c30ed40a0369bd23744cd86f86730
Jaebock jo <last2599@gmail.com>
2021-05-23 08:09:09 +0900

    조합의 경우수(메모이제이션)

---
+++ section08/example12.js
@@ -0,0 +1,14 @@
+function solution(n, r) {
+  let answer = [];
+  let dy = Array.from(Array(n + 1), () => Array(n + 1).fill(0));
+
+  function DFS(n, r) {
+    if (dy[n][r] > 0) return dy[n][r];
+    if (n === r || r === 0) return 1;
+    else return (dy[n][r] = DFS(n - 1, r - 1) + DFS(n - 1, r));
+  }
+  answer = DFS(n, r);
+  return answer;
+}
+
+console.log(solution(33, 19));
