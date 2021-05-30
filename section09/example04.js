c7f9ee289e327c01b1cda86d5f24a2366dcd49d5
Jaebock jo <last2599@gmail.com>
2021-05-30 14:52:15 +0900

    미로탐색(DFS)

---
+++ section09/example04.js
@@ -0,0 +1,34 @@
+function solution(board) {
+  let count = 0;
+  let dx = [1, 0, -1, 0];
+  let dy = [0, -1, 0, 1];
+  function DFS(x = 0, y = 0) {
+    if (x === 6 && y === 6) count++;
+    else {
+      for (let i = 0; i < 4; i++) {
+        let nx = x + dx[i];
+        let ny = y + dy[i];
+        if (nx >= 0 && nx <= 6 && ny >= 0 && ny <= 6 && board[nx][ny] === 0) {
+          board[nx][ny] = 1;
+          DFS(nx, ny);
+          board[nx][ny] = 0;
+        }
+      }
+    }
+  }
+  board[0][0] = 1;
+  DFS(0, 0);
+  return count;
+}
+
+let arr = [
+  [0, 0, 0, 0, 0, 0, 0],
+  [0, 1, 1, 1, 1, 1, 0],
+  [0, 0, 0, 1, 0, 0, 0],
+  [1, 1, 0, 1, 0, 1, 1],
+  [1, 1, 0, 0, 0, 0, 1],
+  [1, 1, 0, 1, 1, 0, 0],
+  [1, 0, 0, 0, 0, 0, 0],
+];
+
+console.log(solution(arr));
