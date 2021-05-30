af3a504036f2533b218f77e9349099fc67e06c42
Jaebock jo <last2599@gmail.com>
2021-05-30 14:56:03 +0900

    이진트리 넓이우선탐색(BFS)

---
+++ section09/example05.js
@@ -0,0 +1,16 @@
+function solution() {
+  let answer = "";
+  let queue = [];
+  queue.push(1);
+  while (queue.length) {
+    let v = queue.shift();
+    answer += v + " ";
+    for (let nv of [v * 2, v * 2 + 1]) {
+      if (nv > 7) continue;
+      queue.push(nv);
+    }
+  }
+  return answer;
+}
+
+console.log(solution());
